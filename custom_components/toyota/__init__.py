c567f472c511692d91af81978fb7b77435b071df
Simon Hörrle <simon.hoerrle@dm.de>
2025-10-02 14:49:28 +0200

    running the coroutines in a new event loop inside the executor thread

--- custom_components/toyota/__init__.py
+++ custom_components/toyota/__init__.py
@@ -52,6 +52,8 @@ from pytoyoda.exceptions import (  # noqa: E402
 )
 
 if TYPE_CHECKING:
+    from collections.abc import Coroutine
+
     from homeassistant.config_entries import ConfigEntry
     from homeassistant.core import HomeAssistant
     from pytoyoda.models.summary import Summary
@@ -73,6 +75,15 @@ class VehicleData(TypedDict):
     data: Vehicle
     statistics: StatisticsData | None
     metric_values: bool
+
+
+def _run_pytoyoda_sync(coro: Coroutine) -> Coroutine:
+    """Run a pytoyoda coroutine in a new event loop."""
+    loop = asyncio.new_event_loop()
+    try:
+        return loop.run_until_complete(coro)
+    finally:
+        loop.close()
 
 
 async def async_setup_entry(  # pylint: disable=too-many-statements # noqa: PLR0915, C901
@@ -113,13 +124,16 @@ async def async_setup_entry(  # pylint: disable=too-many-statements # noqa: PLR0
         """Fetch vehicle data from Toyota API."""
         try:
             vehicles = await asyncio.wait_for(
-                hass.async_add_executor_job(client.get_vehicles), 15
+                hass.async_add_executor_job(_run_pytoyoda_sync, client.get_vehicles()),
+                15,
             )
             vehicle_informations: list[VehicleData] = []
             if vehicles:
                 for vehicle in vehicles:
                     if vehicle:
-                        await hass.async_add_executor_job(vehicle.update)
+                        await hass.async_add_executor_job(
+                            _run_pytoyoda_sync, vehicle.update
+                        )
                         vehicle_data = VehicleData(
                             data=vehicle, statistics=None, metric_values=metric_values
                         )
@@ -128,16 +142,17 @@ async def async_setup_entry(  # pylint: disable=too-many-statements # noqa: PLR0
                             # Use parallel request to get car statistics.
                             driving_statistics = await asyncio.gather(
                                 hass.async_add_executor_job(
-                                    vehicle.get_current_day_summary
+                                    _run_pytoyoda_sync, vehicle.get_current_day_summary
                                 ),
                                 hass.async_add_executor_job(
-                                    vehicle.get_current_week_summary
+                                    _run_pytoyoda_sync, vehicle.get_current_week_summary
                                 ),
                                 hass.async_add_executor_job(
-                                    vehicle.get_current_month_summary
+                                    _run_pytoyoda_sync,
+                                    vehicle.get_current_month_summary,
                                 ),
                                 hass.async_add_executor_job(
-                                    vehicle.get_current_year_summary
+                                    _run_pytoyoda_sync, vehicle.get_current_year_summary
                                 ),
                             )
 
